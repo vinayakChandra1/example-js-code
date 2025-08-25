ae69ae99e29ecce2a8a8a1e6c201509d0cea1cb7
vinayak <220067562+vinayakChandra1@users.noreply.github.com>
2025-08-25 09:48:25 +0000

    Written

---
+++ script.js
@@ -0,0 +1,15 @@
+// Example Code - To test syntax highlighting
+function createGreeting(name) {
+  const greeting = `Hello, ${name}!`;
+  return greeting;
+}
+
+// To check error highlighting
+// function createGreeting(name) {
+//   const greeting = `Hello, ${Name}!`;
+//   return greeting;
+// }
+
+// const helloChris = createGreeting("Chris);
+
+// console.log(helloChris;
